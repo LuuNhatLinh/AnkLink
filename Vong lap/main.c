f73cb5760bbd9a6671d8ddd2653e538f83d6f145
LuuNhatLinh <luulinhdan06@gmail.com>
2025-03-25 10:05:08 +0700

    Add files via upload

---
+++ Vong lap/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ double a;
+ double b=0;
+ printf("Nhap so N: ");
+ scanf("%lf",&a);
+ for(double i=1;i<=a;i++){
+    b+=(1/i);
+ }
+ printf("Tong= %.2lf",b);
+ return 0;
+}
